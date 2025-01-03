201ac800b7350709f9354a57b0ebac16cb3791f4
Karan Oza <karanoza94@gmail.com>
2025-01-03 12:06:30 +0530

    first commit

--- ang-17/ng-18/README.md
+++ ang-17/ng-18/README.md
@@ -1,4 +1,4 @@
-# Ang17
+# Ng18
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
 
--- ang-17/ng-18/src/app/app.component.spec.ts
+++ ang-17/ng-18/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ang-17' title`, () => {
+  it(`should have the 'ng-18' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-17');
+    expect(app.title).toEqual('ng-18');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ang-17');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-18');
   });
 });
