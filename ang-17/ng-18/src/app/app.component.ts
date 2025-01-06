e819dee046317dd12db0508cf7882a50749a4a4f
Karan Oza <karanoza94@gmail.com>
2025-01-06 12:59:29 +0530

    adding signals and state management

--- ang-17/ng-18/src/app/app.component.ts
+++ ang-17/ng-18/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-18';
