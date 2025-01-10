859ff68697a29d6066aa8e683aed41cbd31ba215
Karan Oza <karanoza94@gmail.com>
2025-01-10 12:16:23 +0530

    create a custom task using eventemitter

--- ang-17/ng-18/src/app/user/user/user.component.ts
+++ ang-17/ng-18/src/app/user/user/user.component.ts
@@ -1,11 +1,7 @@
 import { Component,computed, EventEmitter, Input, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
+import { User } from './user.model';
 
-type User = {
-  id: string;
-  avatar: string;
-  name: string;
-};
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
@@ -16,6 +12,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
 
